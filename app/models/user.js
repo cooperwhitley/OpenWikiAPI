473ccdb98a88c060819a8549887eb6124f239ed3
cooperwhitley <whitleycooperr@gmail.com>
2023-10-10 12:41:49 -0500

    change export type to model instead of schema

--- app/models/user.js
+++ app/models/user.js
@@ -33,10 +33,6 @@ const userSchema = new mongoose.Schema(
 	}
 )
 
-// const User = mongoose.model('User', userSchema)
 
-// exporting the schema only as it allows me to reference multiple users as editors for articles
-// making the conversion to a model within /app/routes/user_routes.js should (?) work
-// as in traditional usage I believe that's the only place it should really come up
-// as owner references a schema instead
-module.exports = userSchema
+
+module.exports = mongoose.model('User', userSchema)
